8a42d86f0babb419484209c189797b629b9048c3
diodonfrost <diodon.frost@diodonfrost.me>
2019-05-03 19:56:01 +0200

    add inspec tests

---
+++ tests/inspec/ssm_spec.rb
@@ -0,0 +1,23 @@
+# Check if Packer is functionnal
+
+amazon_ssm_package = 'amazon-ssm-agent'
+amazon_ssm_service = 'amazon-ssm-agent'
+
+control 'amazon-ssm-01' do
+  impact 1.0
+  title 'Amazon ssm install'
+  desc 'Amazon ssm agent should be installed'
+  describe package(amazon_ssm_package) do
+    it { should be_installed }
+  end
+end
+
+control 'amazon-ssm-02' do
+  impact 1.0
+  title 'Amazon ssm service'
+  desc 'Amazon ssm agent service should be started and enabled'
+  describe service(amazon_ssm_service) do
+    it { should be_enabled }
+    it { should be_running }
+  end
+end
